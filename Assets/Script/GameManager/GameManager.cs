21e36de9f0ea08f27da0db48539a34aaa78e1026
lurenhu <109047008+lurenhu@users.noreply.github.com>
2024-06-01 12:50:50 +0800

    商店UI与BUFF商品UI

--- Assets/Script/GameManager/GameManager.cs
+++ Assets/Script/GameManager/GameManager.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class GameManager : SingletonMonobehaviour<GameManager>
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
